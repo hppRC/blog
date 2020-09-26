61b785ee3ba7f87082d3cfaa293c37ab1a6816ac
hppRC <hpp.ricecake@gmail.com>
2020-09-27 01:46:49 +0900

    :+1: add 100vh solution

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import Div100vh from 'react-div-100vh';
 
 import { Footer } from './footer';
 import { Header } from './header';
 
 export const Layout: React.FC = ({ children }) => (
-  <>
+  <Div100vh className='flex flex-col'>
     <Header />
     <main className='flex-1 max-w-screen-xl w-full mx-auto px-2'>{children}</main>
     <Footer />
-  </>
+  </Div100vh>
 );
